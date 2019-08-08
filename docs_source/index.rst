5da9921892a3c39845759ce654d153e13d7f6419
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-08-09 01:04:41 +0200

    Add package infrastructure

---
+++ docs_source/index.rst
@@ -0,0 +1,18 @@
+lihzahrd
+====================================
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. automodule:: lihzahrd
+   :members:
+   :private-members:
+   :undoc-members:
+
+
+Some useful links
+------------------------------------
+
+* `Lihzahrd on GitHub <https://github.com/Steffo99/lihzahrd>`_
+* :ref:`genindex`
---
+++ setup.py
@@ -0,0 +1,26 @@
+import royalnet.version
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="lihzahrd",
+    version="1.0a1",
+    author="Stefano Pigozzi",
+    author_email="ste.pigozzi@gmail.com",
+    description="A Terraria world parser in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Steffo99/lihzahrd",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7"
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
