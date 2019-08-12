8cf9dcdd36c6695dde19c801d244fa80b4d50687
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-08-12 23:15:11 +0200

    Fix doc bug

--- lihzahrd/tileentities/itemframe.py
+++ lihzahrd/tileentities/itemframe.py
@@ -2,6 +2,8 @@ from ..chests import ItemStack
 
 
 class ItemFrame:
+    """Data pertaining to an https://terraria.gamepedia.com/Item_Frame ."""
+
     __slots__ = "item"
 
     def __init__(self, item: ItemStack):
