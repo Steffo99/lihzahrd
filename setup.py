d5034df74d8c096584ee8ace053fab15f54c8979
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-08-19 14:09:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     install_requires=[],
     python_requires=">=3.7",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.7"
     ],
