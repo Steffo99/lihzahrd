aab014f2ed1120fff92be2ac707a41fe600dbaa8
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-08-21 13:50:01 +0200

    Revert "Allow unknown slopes"
    This reverts commit df903a904a8f2538982ce87ff686a3f5fe348b82.

--- lihzahrd/tiles/shape.py
+++ lihzahrd/tiles/shape.py
@@ -12,8 +12,6 @@ class Shape(enum.IntEnum):
     TOP_LEFT_SLOPE = 3
     BOTTOM_RIGHT_SLOPE = 4
     BOTTOM_LEFT_SLOPE = 5
-    UNKNOWN_6 = 6
-    UNKNOWN_7 = 7
 
     @classmethod
     def from_flags(cls, flags2):
