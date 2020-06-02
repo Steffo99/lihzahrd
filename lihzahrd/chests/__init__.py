a1bf13b801cb2c0efd06d315f49dcec9c6d978fb
Dale Floer <dalefloer@gmail.com>
2020-06-01 17:30:05 -0700

    Mannequin parsing.

--- lihzahrd/chests/__init__.py
+++ lihzahrd/chests/__init__.py
@@ -1,5 +1,6 @@
 from .itemtype import ItemType
 from .itemstack import ItemStack
 from .chest import Chest
+from .mannequin import Mannequin
 
-__all__ = ["ItemType", "ItemStack", "Chest"]
+__all__ = ["ItemType", "ItemStack", "Chest", "Mannequin"]
