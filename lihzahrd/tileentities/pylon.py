101fdf0da941f2d78699c6ad1b5395b7338eee42
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-03 14:32:38 +0200

    Remove unused bool from pylons

--- lihzahrd/tileentities/pylon.py
+++ lihzahrd/tileentities/pylon.py
@@ -1,8 +1,5 @@
 class Pylon:
     """Data pertaining to a Pylon (https://terraria.gamepedia.com/Pylons)"""
 
-    def __init__(self, pylon: bool = True):
-        self.pylon: bool = pylon
-
     def __repr__(self):
-        return f"pylon={self.pylon}"
+        return f"Pylon()"
