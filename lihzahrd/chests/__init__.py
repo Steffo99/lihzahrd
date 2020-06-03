c0adcef396efeeae953146af25eab8809a615213
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-03 15:05:36 +0200

    Create Mannequin and HatRack classes

--- lihzahrd/chests/__init__.py
+++ lihzahrd/chests/__init__.py
@@ -1,7 +1,7 @@
 from .itemtype import ItemType
 from .itemstack import ItemStack
 from .chest import Chest
-from .clothingdisplay import ClothingDisplay
+from .clothingdisplay import ClothingDisplay, Mannequin, HatRack
 from .singleitemdisplay import SingleItemDisplay
 
-__all__ = ["ItemType", "ItemStack", "Chest", "ClothingDisplay", "SingleItemDisplay"]
+__all__ = ["ItemType", "ItemStack", "Chest", "ClothingDisplay", "SingleItemDisplay", "Mannequin", "HatRack"]
