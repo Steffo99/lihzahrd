cddc0b7315c7ec2ccd0cfd26a6bcc694c7d593a6
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-06 18:59:37 +0200

    Fix typo

--- lihzahrd/header/treetopvariants.py
+++ lihzahrd/header/treetopvariants.py
@@ -1,9 +1,10 @@
 import typing
 
+
 class TreetopVariants:
-    """Information about the treetop variants in the worls"""
-    def __init__(self,
-        treetop_variants: typing.List[str]):
+    """(Unknown) Information about the treetop variants in the world."""
+
+    def __init__(self, treetop_variants: typing.List[str]):
         self.treetop_variants: typing.List[str] = treetop_variants
 
     def __repr__(self):
