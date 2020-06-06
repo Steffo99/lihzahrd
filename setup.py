d5309fe8468d0ecfaf9a4556d53309582331883a
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-06 19:02:46 +0200

    Bump the version number to 2.0a1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="lihzahrd",
-    version="1.0b6",
+    version="2.0a1",
     author="Stefano Pigozzi",
     author_email="ste.pigozzi@gmail.com",
     description="A Terraria world parser in Python",
