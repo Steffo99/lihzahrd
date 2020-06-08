f8dd891538bf5b80f71322a9ba9697fc700bf04f
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-08 02:56:13 +0200

    Fix type annotation

--- lihzahrd/header/treetopvariants.py
+++ lihzahrd/header/treetopvariants.py
@@ -4,8 +4,8 @@ import typing
 class TreetopVariants:
     """(Unknown) Information about the treetop variants in the world."""
 
-    def __init__(self, treetop_variants: typing.List[str]):
-        self.treetop_variants: typing.List[str] = treetop_variants
+    def __init__(self, treetop_variants: typing.List[int]):
+        self.treetop_variants: typing.List[int] = treetop_variants
 
     def __repr__(self):
         return f"WorldTreetopVariants({self.treetop_variants})"
