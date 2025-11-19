735daae90fc65bc5092a7c9da4aa3decfef42915
Stefano Pigozzi <me@steffo.eu>
2025-11-19 05:15:56 +0100

    Fix Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,64 +1,50 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
+import datetime
 
 project = "lihzahrd"
-copyright = "2020, Stefano Pigozzi"
 author = "Stefano Pigozzi"
+project_copyright = f'{datetime.date.today().year}, {author}'
 
-# -- General configuration ---------------------------------------------------
+language = "en"
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.intersphinx"]
+html_theme_options = {
+    # Set this to the main color of your project
+    "style_nav_header_background": "#7FA23E",
+}
+html_context = {}
 
-intersphinx_mapping = {"python": ("https://docs.python.org/3.6", None)}
+extensions = [
+    "sphinx.ext.napoleon", 
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.13", None),
+}
+manpages_url = "https://man.archlinux.org/"
+
 napoleon_use_param = True
-
 
 def skip(app, what, name: str, obj, would_skip, options):
     if name == "__init__" or name == "__getitem__" or name == "__getattr__":
         return not bool(obj.__doc__)
     return would_skip
 
+todo_include_todos = True
+todo_emit_warnings = True
+todo_link_only = False
 
-def setup(app):
-    app.add_stylesheet("lihzahrd.css")
-
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_theme = 'sphinx_rtd_theme'
+html_title = f"{project}"
+html_short_title = f"{project}"
+html_static_path = [
+    "_static",
+]
+html_extra_path = [
+    "_extra",
+]
+html_domain_indices = False
