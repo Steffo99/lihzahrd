30ef8e4ffd2c8a0e1cfbc2192080263e3226f9cc
Stefano Pigozzi <me@steffo.eu>
2025-11-20 01:02:34 +0100

    Remove typing import in journeypowers

--- src/lihzahrd/journeypowers/journeypowers.py
+++ src/lihzahrd/journeypowers/journeypowers.py
@@ -1,6 +1,3 @@
-import typing
-
-
 class JourneyPowers:
     """Journey mode powers settings. Spawn rate does not appear to be stored in the world."""
 
@@ -17,30 +14,30 @@ class JourneyPowers:
 
     def __init__(
         self,
-        freeze_time: typing.Optional[bool] = None,
-        time_rate: typing.Optional[float] = None,
-        freeze_rain: typing.Optional[bool] = None,
-        freeze_wind: typing.Optional[bool] = None,
-        difficulty: typing.Optional[float] = None,
-        freeze_biome_spread: typing.Optional[bool] = None,
+        freeze_time: bool|None = None,
+        time_rate: float|None = None,
+        freeze_rain: bool|None = None,
+        freeze_wind: bool|None = None,
+        difficulty: float|None = None,
+        freeze_biome_spread: bool|None = None,
     ):
 
-        self.freeze_time: bool = freeze_time
+        self.freeze_time: bool|None = freeze_time
         """Is time frozen?"""
 
-        self.time_rate: float = time_rate
+        self.time_rate: float|None = time_rate
         """How fast does time go, 1x to 24x. Value ranges from 0.0 to 1.0."""
 
-        self.freeze_rain: bool = freeze_rain
+        self.freeze_rain: bool|None = freeze_rain
         """Can the rain change."""
 
-        self.freeze_wind: bool = freeze_wind
+        self.freeze_wind: bool|None = freeze_wind
         """Can the wind speed and direction change."""
 
-        self.difficulty: float = difficulty
+        self.difficulty: float|None = difficulty
         """Enemy difficulty scaling, 0.5x to 3x. Value ranges from 0.0 to 1.0."""
 
-        self.freeze_biome_spread: bool = freeze_biome_spread
+        self.freeze_biome_spread: bool|None = freeze_biome_spread
         """Can evil biomes & the hallow spread."""
 
     def __repr__(self):
