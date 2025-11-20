91755d6fc3aa8241f397d39cfb801b3b3e29e7ea
Stefano Pigozzi <me@steffo.eu>
2025-11-20 18:07:17 +0100

    Export Packable

--- src/lihzahrd/fileutils/__init__.py
+++ src/lihzahrd/fileutils/__init__.py
@@ -3,5 +3,6 @@ from .coordinates import Coordinates
 from .filepacker import FilePacker
 from .pointers import Pointers
 from .rect import Rect
+from .packable import Packable
 
-__all__ = ["FilePacker", "BITS", "Rect", "Pointers", "Coordinates"]
+__all__ = ["FilePacker", "BITS", "Rect", "Pointers", "Coordinates", "Packable"]
