8a8ae274346e8b29248c851c586f38b2cbb8f860
Stefano Pigozzi <me@steffo.eu>
2025-11-20 18:08:17 +0100

    Mark `Packable.read` and `Packable.write` as abstract

--- src/lihzahrd/fileutils/packable.py
+++ src/lihzahrd/fileutils/packable.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 from typing import Self
 
 from lihzahrd.fileutils import FilePacker
@@ -9,9 +9,11 @@ class Packable(metaclass=ABCMeta):
     An object which can be serialized and deserialized via a :class:`lihzahrd.fileutils.FilePacker`.
     """
 
+    @abstractmethod
     def write(self, f: FilePacker):
         raise NotImplementedError()
 
     @classmethod
+    @abstractmethod
     def read(cls, f: FilePacker) -> Self:
         raise NotImplementedError()
