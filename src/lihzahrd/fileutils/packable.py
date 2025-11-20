12365ff44da728f02c6ee82bee753ddb29e169e1
Stefano Pigozzi <me@steffo.eu>
2025-11-20 18:35:21 +0100

    Do not make `Packable` abstract to avoid conflicts with `Enum`

--- src/lihzahrd/fileutils/packable.py
+++ src/lihzahrd/fileutils/packable.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from abc import ABCMeta, abstractmethod
 from typing import Self, TYPE_CHECKING
 
 from lihzahrd.fileutils import FilePacker
@@ -9,16 +8,14 @@ if TYPE_CHECKING:
     from lihzahrd.header import Version
 
 
-class Packable(metaclass=ABCMeta):
+class Packable:
     """
     An object which can be serialized and deserialized via a :class:`lihzahrd.fileutils.FilePacker`.
     """
 
-    @abstractmethod
     def write(self, f: FilePacker, v: Version | None):
         raise NotImplementedError()
 
     @classmethod
-    @abstractmethod
     def read(cls, f: FilePacker, v: Version | None) -> Self:
         raise NotImplementedError()
