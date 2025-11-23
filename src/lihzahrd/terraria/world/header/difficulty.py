a43987326891f76e386920cacb5ab750979d1f10
Stefano Pigozzi <me@steffo.eu>
2025-11-23 06:21:15 +0100

    Document `Difficulty`

--- src/lihzahrd/terraria/world/header/difficulty.py
+++ src/lihzahrd/terraria/world/header/difficulty.py
@@ -3,9 +3,16 @@ import enum
 
 class Difficulty(enum.IntEnum):
     JOURNEY = 3
+    """Journey mode."""
+
     CLASSIC = 0
+    """Classic mode, or Expert mode in for-the-worthy worlds."""
+
     EXPERT = 1
+    """Expert mode, or Master mode in for-the-worthy worlds."""
+
     MASTER = 2
+    """Master mode, or Legendary mode in for-the-worthy worlds."""
 
     def __repr__(self):
         return f"{self.__class__.__qualname__}.{self.name}"
