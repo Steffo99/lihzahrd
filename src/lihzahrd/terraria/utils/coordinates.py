6ea7cab285e1d521ce9f734f77affcf54a660e3f
Stefano Pigozzi <me@steffo.eu>
2025-11-23 06:19:14 +0100

    Use typing in Coordinates

--- src/lihzahrd/terraria/utils/coordinates.py
+++ src/lihzahrd/terraria/utils/coordinates.py
@@ -1,11 +1,16 @@
+from typing import TypeVar
+
+CoordinateType = TypeVar("CoordinateType")
+
+
 class Coordinates:
     """A pair of coordinates."""
 
     __slots__ = ("x", "y")
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x: CoordinateType, y: CoordinateType):
+        self.x: CoordinateType = x
+        self.y: CoordinateType = y
 
     def __repr__(self):
         x = self.x
