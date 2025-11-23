85fb8812a1b47156b8064693a1bcb754c083afb4
Stefano Pigozzi <me@steffo.eu>
2025-11-23 06:22:06 +0100

    Make `Difficulty` into a `WorldVersionedPackable`

--- src/lihzahrd/terraria/world/header/difficulty.py
+++ src/lihzahrd/terraria/world/header/difficulty.py
@@ -1,7 +1,14 @@
-import enum
+from enum import IntEnum
+from typing import Self
+
+from ..version import Version
+from ..worldversionedpackable import WorldVersionedPackable
+from ...utils import FilePacker
 
 
-class Difficulty(enum.IntEnum):
+class Difficulty(WorldVersionedPackable, IntEnum):
+    """A world's raw difficulty level."""
+
     JOURNEY = 3
     """Journey mode."""
 
@@ -16,3 +23,10 @@ class Difficulty(enum.IntEnum):
 
     def __repr__(self):
         return f"{self.__class__.__qualname__}.{self.name}"
+
+    def serialize(self, f: FilePacker, *, v: Version) -> None:
+        f.write_int4(self.value)
+
+    @classmethod
+    def deserialize(cls, f: FilePacker, *, v: Version) -> Self:
+        return cls(f.read_int4())
